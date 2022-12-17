0fff2bf04f0854636453e834f6c79a6236602c43
Kohta9521 <kohta0421kochi@gmail.com>
2022-12-18 01:12:12 +0900

    add lesson1 2

---
+++ lesson01/sample01.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <title>PHP SQL LESSON</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- css 読み込み部分 -->
+    <link rel="stylesheet" href="style.css">
+</head>
+    <body>
+        <?php 
+            echo "私の名前は";
+        ?>
+</body>
+</html>
